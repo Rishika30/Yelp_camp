28b713ccc7c1dd8d171e5aabb95a80377c3307d9
Rishika30 <agarwalrishika30@gmail.com>
2020-09-03 07:58:00 +0000

    deploy

--- middleware/index.js
+++ middleware/index.js
@@ -56,7 +56,7 @@ middlewareObj.isLoggedIn = function(req,res,next){
 	if(req.isAuthenticated()){
 	   return next();
 	   }
-	req.flash("error", "You need to be loggeed in to do that");
+	req.flash("error", "You need to be logged in to do that");
 	res.redirect("/login");
 }
 
